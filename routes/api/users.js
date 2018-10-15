e50f311ee15ba3cbcca3ff66ca84ccd722cd1ed0
Fernando Rowies <fernandorowies.saxo@gmail.com>
2018-10-15 18:29:58 -0300

    Validation of user Login continued

--- routes/api/users.js
+++ routes/api/users.js
@@ -90,17 +90,12 @@ router.post("/login", (req, res) => {
       if (isMatch) {
         // Generate the token
         const payload = { id: user.id, name: user.name, avatar: user.avatar };
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          { expiresIn: 3600 },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token // Bearer is a protocol for tokens
-            });
-          }
-        );
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 15 }, (err, token) => {
+          res.json({
+            success: true,
+            token: "Bearer " + token // Bearer is a protocol for tokens
+          });
+        });
       } else {
         errors.password = "Password incorrect";
         return res.status(400).json(errors);
