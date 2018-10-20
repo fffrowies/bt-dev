0a09ae9bf96ec641a167cde15f1a72cafa735c59
Fernando Rowies <fernandorowies.saxo@gmail.com>
2018-10-20 09:49:21 -0300

    Profile Credentials done, started PRofile Github

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,10 +1,78 @@
 import React, { Component } from "react";
+import Moment from "react-moment";
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+
+    const experienceItems = experience.map(exp => (
+      <li key={exp._id} className="list-group-item">
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="DD/MM/YYYY">{exp.from}</Moment> -{" "}
+          {exp.to ? <Moment format="DD/MM/YYYY">{exp.to}</Moment> : "Now"}
+        </p>
+        <p>
+          <strong>Position:</strong> {exp.title}
+        </p>
+        <p>
+          {exp.location === "" ? null : (
+            <span>
+              <strong>Location:</strong> {exp.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description === "" ? null : (
+            <span>
+              <strong>Description:</strong> {exp.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
+    const educationItems = education.map(edu => (
+      <li key={edu._id} className="list-group-item">
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="DD/MM/YYYY">{edu.from}</Moment> -{" "}
+          {edu.to ? <Moment format="DD/MM/YYYY">{edu.to}</Moment> : "Now"}
+        </p>
+        <p>
+          <strong>Degree:</strong> {edu.degree}
+        </p>
+        <p>
+          <strong>Field Of S tudy:</strong> {edu.fieldofstudy}
+        </p>
+        <p>
+          {edu.description === "" ? null : (
+            <span>
+              <strong>Description:</strong> {edu.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
     return (
-      <div>
-        <h1>TODO: PROFILE CREDENTIALS</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {experienceItems.length > 0 ? (
+            <ul className="list-group">{experienceItems}</ul>
+          ) : (
+            <p className="text-center">No Experience Listed</p>
+          )}
+        </div>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {educationItems.length > 0 ? (
+            <ul className="list-group">{educationItems}</ul>
+          ) : (
+            <p className="text-center">No Education Listed</p>
+          )}
+        </div>
       </div>
     );
   }
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 class ProfileGithub extends Component {
+  state = {
+    clientId: "d409900a060810237bdf",
+    clientSecret: "f505472f3990d39028050a7740bf156dc4dbea53",
+    count: 5,
+    sort: "created: asc",
+    repos: []
+  };
+
+  componentDidMount() {
+    const { username } = this.props;
+    const { count, sort, clientId, clientSecret } = this.state;
+
+    fetch(
+      `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ repos: data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { repos } = this.state;
+    const repoItems = repos.map(repo => (
+      <div key={repo.id} className="card card-body mb-2">
+        <div className="row">
+          <div className="col-md-6">
+            <h4></h4>          
+          </div>
+        </div>
+      </div>
+    ));
+
     return (
       <div>
         <h1>TODO: PROFILE GITHUB</h1>
